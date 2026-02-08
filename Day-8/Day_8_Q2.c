ac971081a0e9072e6ceac7240c2a4a47cb028306
vatsal-ag <117094352+vatsal-ag@users.noreply.github.com>
2026-02-08 11:24:05 +0530

    Day 8: 100 Days of Code

---
+++ Day-8/Day_8_Q2.c
@@ -0,0 +1,23 @@
+// Given an integer n, return true if it is a power of two. Otherwise, return false.
+// An integer n is a power of two, if there exists an integer x such that n == 2x.
+// Name: Vatsal Agarwal
+// SAP ID: 590023223
+
+#include <stdbool.h>
+
+bool isPowerOfTwo(int n)
+{
+    if (n == 0)
+    {
+        return false;
+    }
+    if (n == 1)
+    {
+        return true;
+    }
+    if (n % 2 != 0)
+    {
+        return false;
+    }
+    return isPowerOfTwo(n / 2);
+}
