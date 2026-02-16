4f29d6e09a5ffdfeb613676b3de8765767a3c3f5
vatsal-ag <117094352+vatsal-ag@users.noreply.github.com>
2026-02-16 20:15:48 +0530

    Day 16: 100 Days of Code

---
+++ Day-16/Day_16_Q1.c
@@ -0,0 +1,43 @@
+// Given an array of integers, count the frequency of each distinct element and print the result.
+// Name: Vatsal Agarwal
+// SAP ID: 590023223
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter elements of array:\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("Element[%d]: ", i);
+        scanf("%d", &arr[i]);
+    }
+    int freq[n];
+    for (int i = 0; i < n; i++)
+    {
+        freq[i] = 1;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                freq[i]++;
+                for (int k = j; k < n - 1; k++)
+                {
+                    arr[k] = arr[k + 1];
+                }
+                n--;
+                j--;
+            }
+        }
+    }
+    printf("Frequency of each distinct element:\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d: %d\n", arr[i], freq[i]);
+    }
+    return 0;
+}
---
+++ Day-16/Day_16_Q2.c
@@ -0,0 +1,21 @@
+// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+// Name: Vatsal Agarwal
+// SAP ID: 590023223
+
+void rotate(int *nums, int numsSize, int k)
+{
+    k = k % numsSize;
+    int temp[k];
+    for (int i = 0; i < k; i++)
+    {
+        temp[i] = nums[numsSize - k + i];
+    }
+    for (int i = numsSize - 1; i >= k; i--)
+    {
+        nums[i] = nums[i - k];
+    }
+    for (int i = 0; i < k; i++)
+    {
+        nums[i] = temp[i];
+    }
+}
