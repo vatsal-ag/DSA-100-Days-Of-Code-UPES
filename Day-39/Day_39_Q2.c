0345e2b0b7749575c208b36f0ae3e55c3e785f6e
Vatsal Agarwal <vatsalagarwal.2001@gmail.com>
2026-03-11 23:15:51 +0530

    Add files via upload

---
+++ Day-39/Day_39_Q2.c
@@ -0,0 +1,58 @@
+// Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+// Name: Vatsal Agarwal
+// SAP ID: 590023223
+
+#include <stdlib.h>
+
+typedef struct
+{
+    int val;
+    int count;
+} Element;
+
+int compareCounts(const void *a, const void *b)
+{
+    Element *elemA = (Element *)a;
+    Element *elemB = (Element *)b;
+    return elemB->count - elemA->count;
+}
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int *topKFrequent(int *nums, int numsSize, int k, int *returnSize)
+{
+
+    int hash[20001] = {0};
+    for (int i = 0; i < numsSize; i++)
+    {
+        hash[nums[i] + 10000]++;
+    }
+
+    Element *uniqueElements = (Element *)malloc(sizeof(Element) * numsSize);
+    int uniqueCount = 0;
+
+    for (int i = 0; i < 20001; i++)
+    {
+        if (hash[i] > 0)
+        {
+            uniqueElements[uniqueCount].val = i - 10000;
+            uniqueElements[uniqueCount].count = hash[i];
+            uniqueCount++;
+        }
+    }
+
+    qsort(uniqueElements, uniqueCount, sizeof(Element), compareCounts);
+
+    *returnSize = k;
+    int *result = (int *)malloc(sizeof(int) * k);
+
+    for (int i = 0; i < k; i++)
+    {
+        result[i] = uniqueElements[i].val;
+    }
+
+    free(uniqueElements);
+
+    return result;
+}
