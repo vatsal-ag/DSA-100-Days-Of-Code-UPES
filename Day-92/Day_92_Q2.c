581cee2cd4f08e78f8209cdd7f088c9ceef7ba29
vatsal-ag <117094352+vatsal-ag@users.noreply.github.com>
2026-05-03 10:45:56 +0530

    Day 92: 100 Days of Code

---
+++ Day-92/Day_92_Q2.c
@@ -0,0 +1,52 @@
+// Given an integer array nums and an integer k, return the kth largest element in the array.
+// Note that it is the kth largest element in the sorted order, not the kth distinct element.
+// Can you solve it without sorting?
+// Name: Vatsal Agarwal
+// SAP ID: 590023223
+
+#include <stdio.h>
+
+void swap(int *nums, int i, int j)
+{
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
+
+int partition(int *nums, int low, int high)
+{
+    int pivot = nums[low];
+    int j = low;
+
+    for (int i = low + 1; i <= high; i++)
+    {
+        if (nums[i] < pivot)
+        {
+            j++;
+            swap(nums, i, j);
+        }
+    }
+    swap(nums, low, j);
+    return j;
+}
+
+int QSelect(int *nums, int low, int high, int k)
+{
+    if (low <= high)
+    {
+        int p = partition(nums, low, high);
+
+        if (p == k)
+            return nums[p];
+        if (p < k)
+            return QSelect(nums, p + 1, high, k);
+        else
+            return QSelect(nums, low, p - 1, k);
+    }
+    return nums[low];
+}
+
+int findKthLargest(int *nums, int numsSize, int k)
+{
+    return QSelect(nums, 0, numsSize - 1, numsSize - k);
+}
